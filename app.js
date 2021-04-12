2067b5bf7112f2ef4a487e205139ae114bf6a720
Alae Edine Waladi <32775934+alaawaladi@users.noreply.github.com>
2021-04-12 18:43:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function clock(){
-    let hours = document.getElementById('hours')
+    let hour = document.getElementById('hours');
+    let minut = document.getElementById('minuts');
+    let second = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    var am = "AM";
+
+    hour.innerHTML	= h;
+    minut.innerHTML = m;
+    second.innerHTML = s;
+    
 }
+
+var interval = setInterval(clock,1000);
